8c51cb0a85cdbbba271476355ad48c1e3aba821d
nelson <nelson@talentcircles.com>
2015-09-11 18:08:08 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET Hello World page. */
+router.get('/helloWorld', function(req, res) {
+  res.render('helloWorld', { title: 'Hello, World!' });
+});
 module.exports = router;
