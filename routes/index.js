96d01128023aa32a32a2e30b5407f8ec57942dc2
nelson <nelson@talentcircles.com>
2015-09-14 17:35:41 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,13 @@ router.get('/', function(req, res) {
 router.get('/helloWorld', function(req, res) {
   res.render('helloWorld', { title: 'Hello, World!' });
 });
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
 module.exports = router;
